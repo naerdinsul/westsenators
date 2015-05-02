6ca77dc1b3b1060632f0dc33a1f2b4dd01a204fd
William Deacon <naerdinsul@gmail.com>
2015-05-01 20:02:41 -0400

    Remove nivo slider (unused).  Update template layout.  Update graphics.

--- index.php
+++ index.php
@@ -8,101 +8,89 @@ $user            = JFactory::getUser();
 $this->language  = $doc->language;
 $this->direction = $doc->direction;
 
-// Include CSS Stylesheets
-$doc->addStyleSheet( $this->baseurl . '/templates/' . $this->template . '/css/style.css');
-$doc->addStyleSheet( $this->baseurl . '/templates/' . $this->template . '/css/unsemantic-grid-responsive.css');
+$sitename = $app->get( 'sitename' );
 
-?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+// Include Header Information
+$doc->addStyleSheet( 'templates/' . $this->template . '/css/style.css' );
+$doc->addStyleSheet( 'templates/' . $this->template . '/css/unsemantic-grid-responsive.css' );
+$doc->setMetaData( 'viewport', 'width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1' );
+
+?><!DOCTYPE html>
 <html>
 <head>
 <jdoc:include type="head" />
-	<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1" />
-
-	<!-- JQuery functions -->
-	<script type="text/javascript">
-		$(document).ready(function() {
-			$('#mobileNav').children('li').each(function() {
-				$(this).click(function() {
-					/* Hide everything */
-					$('#mobileNav').children('li').not(this).children('ul').each(function() {
-						$(this).hide();
-					});
-				
-					/* Toggle the currently clicked item */
-					$(this).children('ul').toggle();
-				});
-			});
-		});
-    </script>
 </head>
 
 <body>
+	<jdoc:include type="modules" name="notification-position" />
 
-	<!-- Header -->
 	<div id="header-wrapper">
-		<jdoc:include type="modules" name="header" style="xhtml" />
-		<div id="header">
-		<a href="/"><img src="/templates/westsenators/images/header-wp-logo.png" style="margin: 20px;"></a>
+		<div id="header" >
+			<a href="/"><img src="/templates/westsenators/images/header-wp-logo.png" style="margin: 20px;"></a>
 		</div>
+		<jdoc:include type="modules" name="header-position" title="Header" />
 	</div>
-	
-	<!-- Navigation Menu -->
-	<jdoc:include type="modules" name="navmenu" style="xhtml" />
+
 	<div id="navdropshadow"></div>
 	
-	<!-- Site Body -->
+	<!-- Site Body -->	
 	<div id="sitebody" class="grid-container">
-			<!-- Breadcrumb -->
-			<div id="breadcrumb">
-				<jdoc:include type="modules" name="breadcrumb" />
-			</div>
+
+			<jdoc:include type="modules" name="top-position" />
 			
 			<!-- Content on Desktop -->
-			<div class="hide-on-mobile">
-				<div class="grid-25" style="min-height: 100px">
-					<jdoc:include type="modules" name="leftmenu" />
-				</div>
-				
-				<div class="grid-75" style="min-height: 100px">
-					<jdoc:include type="component" />
-				</div>
+			<div class="grid-25" style="min-height: 100px">
+				<jdoc:include type="modules" name="left-position" />
 			</div>
-
-			<!-- Content on Mobile -->
-			<div class="hide-on-desktop">
-				<div class="grid-100" style="min-height: 1000px">
-					<jdoc:include type="component" />
-					<jdoc:include type="modules" name="leftmenu" />
+			
+			<div class="grid-75" style="min-height: 100px">
+				<jdoc:include type="component" />
+			</div>
+			
+			<!-- Footer -->
+			<div id="footer" style="background-color: rgb(58, 58, 58); color: white; clear:both; margin: 0 -10px;">
+			
+				<div style="width: 50%">
+					<div style="padding: 10px; float: left"><img src="/templates/westsenators/images/wp-logo-small.png"></div>
+					<div style="width: 33.333%; float: left">
+						Washington-Nile Local Schools<br>
+						15332 US Hwy. 52<br>
+						West Portsmouth, OH 45663<br>
+					</div>
+					<div style="width: 33.333%; float: left">
+						Phone: (740)-858-1111<br>
+						Fax: (740)-858-1110<br>
+					</div>
+				</div>
+			
+			
+			
+			<!--	<div class="grid-container" style="padding: 30px 0px;">
+					<jdoc:include type="modules" name="bottom-position" />
+					<div class="grid-50 center">
+						<img src="/templates/westsenators/images/header-wp-logo.png" alt="" />
+					</div>
+					<div class="grid-50 center">
+						<address>
+						Washington-Nile Local Schools<br>
+						15332 US Hwy. 52<br>
+						West Portsmouth, OH 45663<br>
+						phone: (740)-858-1111<br>
+						fax: (740)-858-1110<br>
+						</address>
+					</div>
+				</div> -->
+				<div class="grid-container" style="background-color: rgb(23, 23, 23); clear: both;">
+					<div class="copyright">
+						&copy; <?php echo date('Y'); ?> <?php echo $sitename; ?>
+					</div>
 				</div>
 			</div>
 	</div>
 	
-	<!-- Footer -->
-	<div id="footer" style="background: url(/templates/westsenators/images/bg-footer.png) repeat; border-top: 1px solid white; padding: 30px;">
-	<div class="grid-container">
-		<!--<jdoc:include type="modules" name="footer" style="xhtml" />-->
-		<div class="grid-33 center">
-			<img src="/templates/westsenators/images/header-wp-logo.png" alt="" />
-		</div>
-		<div class="grid-33 center">
-			<div>
-			Washington-Nile Local Schools<br>
-			15332 US Hwy. 52<br>
-			West Portsmouth, OH 45663<br>
-			</div>
-		</div>
-		<div class="grid-33 center">
-			Jeff Stricklett, Superintendent<br>
-			Sherry Patterson, Treasurer<br>
-			phone: (740)-858-1111<br>
-			fax: (740)-858-1110<br>
-		</div>
-	</div>
-	<div class="grid-container">
-		<div class="copyright">
-			&copy;2014, Washington-Nile Local Schools
-		</div>
-	</div>
-	</div>
+	
+	
+	
+	<jdoc:include type="modules" name="debug-position" />
 </body>
 </html>

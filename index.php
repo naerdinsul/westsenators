a3d2dadb19ef53f01e3987e7c6d01a25c9fc5986
William Deacon <naerdinsul@gmail.com>
2015-05-01 22:54:46 -0400

    Realign layout template to use Simple Grid instead of Unsemantic-Grid CSS

--- index.php
+++ index.php
@@ -11,8 +11,8 @@ $this->direction = $doc->direction;
 $sitename = $app->get( 'sitename' );
 
 // Include Header Information
+$doc->addStyleSheet( 'templates/' . $this->template . '/css/simplegrid.css' );
 $doc->addStyleSheet( 'templates/' . $this->template . '/css/style.css' );
-$doc->addStyleSheet( 'templates/' . $this->template . '/css/unsemantic-grid-responsive.css' );
 $doc->setMetaData( 'viewport', 'width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1' );
 
 ?><!DOCTYPE html>
@@ -24,73 +24,83 @@ $doc->setMetaData( 'viewport', 'width=device-width, initial-scale=1, minimum-sca
 <body>
 	<jdoc:include type="modules" name="notification-position" />
 
-	<div id="header-wrapper">
-		<div id="header" >
+	<!-- HEADER SECTION -->
+	<div id="header">
+		<div class="grid">
+			<!-- WNLS LOGO -->
 			<a href="/"><img src="/templates/westsenators/images/header-wp-logo.png" style="margin: 20px;"></a>
+			<!-- Other stuff would go here -->
 		</div>
+		
+		<!-- OTHER HEADER MODULES -->
 		<jdoc:include type="modules" name="header-position" title="Header" />
 	</div>
 
-	<div id="navdropshadow"></div>
-	
-	<!-- Site Body -->	
-	<div id="sitebody" class="grid-container">
+	<!-- DROPSHADOW -->
+	<div id="dropshadow"></div>
 
-			<jdoc:include type="modules" name="top-position" />
-			
-			<!-- Content on Desktop -->
-			<div class="grid-25" style="min-height: 100px">
+	<!-- CONTENT SECTION -->	
+	<div id="content" class="grid">
+		
+		<?php if($this->countModules( 'top-position' )) : ?>
+		<!-- TOP CONTENT SECTION -->
+		<div class="grid grid-pad">
+			<div class="col-1-1">
+				<jdoc:include type="modules" name="top-position" />
+			</div>
+		</div>
+		<?php endif; ?>
+		
+		<div class="grid grid-pad">
+			<?php if($this->countModules( 'left-position' )) : ?>
+			<!-- LEFT CONTENT SECTION -->
+			<div class="col-3-12">
 				<jdoc:include type="modules" name="left-position" />
 			</div>
 			
-			<div class="grid-75" style="min-height: 100px">
+			<!-- MAIN CONTENT SECTION -->
+			<div class="col-9-12">			
+			<?php else: ?>
+			<div class="col-1-1">
+			<?php endif; ?>
 				<jdoc:include type="component" />
 			</div>
-			
-			<!-- Footer -->
-			<div id="footer" style="background-color: rgb(58, 58, 58); color: white; clear:both; margin: 0 -10px;">
-			
-				<div style="width: 50%">
-					<div style="padding: 10px; float: left"><img src="/templates/westsenators/images/wp-logo-small.png"></div>
-					<div style="width: 33.333%; float: left">
-						Washington-Nile Local Schools<br>
-						15332 US Hwy. 52<br>
-						West Portsmouth, OH 45663<br>
-					</div>
-					<div style="width: 33.333%; float: left">
-						Phone: (740)-858-1111<br>
-						Fax: (740)-858-1110<br>
-					</div>
-				</div>
-			
-			
-			
-			<!--	<div class="grid-container" style="padding: 30px 0px;">
-					<jdoc:include type="modules" name="bottom-position" />
-					<div class="grid-50 center">
-						<img src="/templates/westsenators/images/header-wp-logo.png" alt="" />
-					</div>
-					<div class="grid-50 center">
-						<address>
-						Washington-Nile Local Schools<br>
-						15332 US Hwy. 52<br>
-						West Portsmouth, OH 45663<br>
-						phone: (740)-858-1111<br>
-						fax: (740)-858-1110<br>
-						</address>
-					</div>
-				</div> -->
-				<div class="grid-container" style="background-color: rgb(23, 23, 23); clear: both;">
-					<div class="copyright">
-						&copy; <?php echo date('Y'); ?> <?php echo $sitename; ?>
-					</div>
-				</div>
+		</div>
+		
+		<?php if($this->countModules( 'top-position' )) : ?>
+		<!-- BOTTOM CONTENT SECTION -->
+		<div class="grid grid-pad">
+			<div class="col-1-1">
+				<jdoc:include type="modules" name="bottom-position" />
 			</div>
+		</div>
+		<?php endif; ?>
 	</div>
 	
-	
-	
-	
+	<!-- FOOTER SECTION -->
+	<div id="footer" class="grid">
+		<div class="grid grid-pad">
+			<div class="col-1-3 center">
+				<img src="/templates/westsenators/images/wp-logo-small.png" alt="Porstmouth West logo" />
+			</div>
+			<div class="col-1-3 center">
+				<address>
+					Washington-Nile Local Schools<br>
+					15332 US Hwy. 52<br>
+					West Portsmouth, OH 45663<br>
+				</address>
+			</div>
+			<div class="col-1-3 center">
+				phone: (740)-858-1111<br>
+				fax: (740)-858-1110<br>
+			</div>
+		</div>
+	</div>
+	<div id="copyright" class="grid">
+		&copy; <?php echo date('Y'); ?> <?php echo $sitename; ?>
+	</div>
+
+	<!-- DEBUG SECTION -->
 	<jdoc:include type="modules" name="debug-position" />
 </body>
 </html>
